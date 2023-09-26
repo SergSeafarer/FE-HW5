f342439bb6a6ce3ed8c41ec449f956abe0bde5cd
Sergii <seregaonma@gmail.com>
2023-09-26 12:50:55 +0300

    complete task

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const firstNumber = prompt('Please, enter first number');
+const secondNumber = prompt('Please, enter second number');
+const thirdNumber = prompt('Please, enter third number');
+const average = (Number(firstNumber) + Number(secondNumber) + Number(thirdNumber)) / 3;
+alert('Your average is: ' + average);
