12dd11eab48a16186e99b0a284d19235b8caab03
Sergii <seregaonma@gmail.com>
2023-09-26 19:52:11 +0300

    check entered value for NaN

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const firstNumber = prompt('Please, enter first number');
-const secondNumber = prompt('Please, enter second number');
-const thirdNumber = prompt('Please, enter third number');
-const average = (Number(firstNumber) + Number(secondNumber) + Number(thirdNumber)) / 3;
-alert('Your average is: ' + average);
+const firstNumber = +prompt('Please, enter first number');
+const secondNumber = +prompt('Please, enter second number');
+const thirdNumber = +prompt('Please, enter third number');
+if(isNaN(firstNumber) === true || isNaN(secondNumber) === true || isNaN(thirdNumber) === true) {
+  alert('Your entered value is not a number')
+} else {
+  const average = (firstNumber + secondNumber + thirdNumber) / 3;
+  alert('Your average is: ' + average);
+}
+  
+  
